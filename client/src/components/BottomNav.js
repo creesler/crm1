737bb5cfdd7b5ffebd3d7ea9eb01049549073326
creesler <60352120+creesler@users.noreply.github.com>
2023-01-15 16:14:23 +0800

    removed bottom nav

--- client/src/components/BottomNav.js
+++ client/src/components/BottomNav.js
@@ -18,20 +18,32 @@ const BottomNav = () => {
   return (
     <Box ref={ref}>
             {{
-                0: <ClusterMap />,
-                1: <Rooms />,
+                // 0: 
+                //     <Paper
+                //         elevation={3}
+                //         sx={{position:'center', 
+                //             bottom:0, 
+                //             left:0, 
+                //             right:0, 
+                //             zIndex:2
+                //         }}
+                //         >
+                //     <img src="https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_960,f_auto/DCTM_Penguin_UK_DK_AL526630_wkmzns.jpg" style={{height:100, width:100, justifyContent:'center'}}/>,
+                //     </Paper>,
+                // 0: <ClusterMap />,
+                // 1: <Rooms />,
                 2: (
                     <Protected>
                         <AddRoom />
                     </Protected>
                 ),
-                3: (
+                // 3: (
                     
-                        <AddProject />
+                //         <AddProject />
                     
-                ),   
+                // ),   
             }[section]}
-        <Paper
+        {/* <Paper
         elevation={3}
         sx={{position:'fixed', bottom:0, left:0, right:0, zIndex:2}}
         >
@@ -45,7 +57,7 @@ const BottomNav = () => {
                 <BottomNavigationAction label='Add' icon={<AddLocationAlt />} />
                 <BottomNavigationAction label='Add' icon={<AddTask />} />
             </BottomNavigation>
-        </Paper>
+        </Paper> */}
     </Box>
   )
 }
